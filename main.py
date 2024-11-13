455e2f06db2e667191a56a7aecc1eb6da08d4e20
OneManEngi <92920307+OneManEngi@users.noreply.github.com>
2024-11-13 22:22:25 +0000

    0.1 Added The Time QDateTime (Should work on it)

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print('hello world')    
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+)
+from PySide6.QtCore import QDateTime
+
+t = QDateTime.currentDateTimeUtc()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Daniel's Clock")
+        label = QLabel(f"{t}")
+
+        self.setCentralWidget(label)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
